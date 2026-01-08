76f66f2ed647142742fb678d8490df57a95f25d1
Thomas Benard <thomasagadabenard@gmail.com>
2026-01-08 19:41:00 +0100

    to-do list

---
+++ second week of January,2026/script.js
@@ -0,0 +1,36 @@
+let add = document.querySelector(".add-btn");
+
+let toDo = document.querySelector(".to-do");
+
+let input = document.querySelector("input");
+
+
+function creattodo(){
+    let para = document.createElement("div");
+    let del = document.createElement("button");
+    del.innerHTML = "Delete";
+    let paragraph = document.createElement("p");
+    para.classList.add("tasks");
+    del.classList.add("del");
+    paragraph.innerText = input.value;
+    toDo.appendChild(para);
+    para.appendChild(paragraph);
+    para.appendChild(del);
+    del.addEventListener("click", () => {
+    para.remove();
+    });
+
+    input.value = "";
+}
+
+function whether(){
+    if (input.value === ""){
+        return
+    } else{
+        creattodo();
+    }
+}
+
+add.addEventListener("click", () => {
+    whether();    
+});
