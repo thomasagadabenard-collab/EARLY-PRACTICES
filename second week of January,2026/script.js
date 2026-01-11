45f52b3538a4e1e3d63e8044a0b8fcb9ceaf66f0
Thomas Benard <thomasagadabenard@gmail.com>
2026-01-11 19:51:03 +0100

    rest for second week of Jan

--- second week of January,2026/script.js
+++ second week of January,2026/script.js
@@ -49,3 +49,118 @@ shareBtn.addEventListener("click", (e) => {
 window.addEventListener("click",() => {
     shareTool.classList.remove("active");
 })
+
+
+const students = [
+    { name: "Alice", score: 85 },
+    { name: "Bob", score: 42 },
+    { name: "Charlie", score: 73 },
+    { name: "David", score: 90 },
+];
+
+const totalScore = students.reduce((sum, student) => sum + student.score, 0);
+const averageScore = totalScore / students.length;
+
+console.log(averageScore);
+
+
+
+let high = students.reduce((accumulator, currentValue) =>{
+    return Math.max(accumulator, currentValue.score)
+
+}, -Infinity)
+
+console.log(high);
+
+
+let min = students.reduce((accumulator, currentValue) =>{
+    return Math.min(accumulator, currentValue.score)
+}, Infinity)
+
+console.log(min);
+
+
+let filt = students.filter(student => student.score >= 50);
+console.log(filt);
+
+
+let mapp = students.map(student => {
+    if(student.score >= 70){
+        console.log("You got an A");
+        
+
+    } else if (student.score >= 60){
+        console.log("You got a B");
+    } else{
+        console.log("Try again");
+        
+    };
+})
+
+
+
+const account = {
+  name: "John Doe",
+  balance: 1000,
+  transactions: [],
+  
+  deposit: function() {
+    let amount = 500;
+    this.balance += amount;
+    this.transactions.push(amount);
+    return console.log(this.balance);
+  },
+  withdraw:function() {
+    let amount =400;
+    this.balance -= amount;
+    this.transactions.push(amount);
+    return console.log(this.balance);
+    
+  },
+  checkBalance: function() {
+    return console.log(this.balance);
+  }
+
+};
+
+account.deposit();
+account.withdraw();
+account.checkBalance();
+console.log(account.transactions);
+
+
+let expenses = [];
+
+function addExpense(amount, description){
+    let popp = {amount, description};
+    expenses.push(popp);
+}
+
+addExpense(500, "sugar");
+addExpense(200, "bread");
+addExpense(1000, "garri");
+console.log(expenses);
+
+function calculateTotal(){
+    let total = expenses.reduce((accumulator, currentValue) => currentValue.amount + accumulator, 0)
+    return console.log((total));
+    
+}
+
+calculateTotal();
+
+function monthlySavings(income){
+    let totalExpenses = expenses.reduce((accumulator, currentValue) => currentValue.amount + accumulator, 0);
+    let savings = income - totalExpenses;
+    return console.log(savings);
+}
+
+monthlySavings(2000);
+
+function formatCurrency(amount){
+    let total = `$${amount.toLocaleString()}.00`;
+    return total;
+}
+
+console.log(formatCurrency(3000));
+ 
