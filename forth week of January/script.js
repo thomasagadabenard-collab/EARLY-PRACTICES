2eb79ee4ba38e831e096eddb04b8ece475675cf6
Thomas Benard <thomasagadabenard@gmail.com>
2026-01-25 12:08:49 +0100

    call back and closure pracice

--- forth week of January/script.js
+++ forth week of January/script.js
@@ -31,3 +31,84 @@ uploadBtn.addEventListener("click", () => {
     entry.value = "";
     uploadInput.value = "";
 });
+
+/*Closure*/
+function createCounter(){
+    let count = 0;
+
+    return function (){
+        count ++
+        console.log(count);
+    }
+}
+
+let newCounter = createCounter()
+
+newCounter();
+newCounter();
+newCounter();
+
+/*Call back function*/
+
+function processNumber(number, callBack){
+    let result = number * 2;
+    callBack(result)
+}
+
+function callBack(result) {
+    return console.log(result);
+}
+
+processNumber(5, callBack);
+
+
+function createAccount(){
+    let balance = 0;
+
+    return {
+        deposit: function(amount){
+            balance += amount;
+            return balance;
+            
+        },
+        withdraw: function (amount){
+            balance -= amount;
+            return balance;
+        }
+    }
+}
+
+const myAccount = createAccount()
+
+console.log(myAccount.deposit(500));
+
+console.log(myAccount.withdraw(200));
+
+
+
+function greetUser(name) {
+
+    return function time(timeOfDay){
+        return `Good ${timeOfDay}, ${name}`
+    }
+    
+}
+
+let newgreet = greetUser("John");
+
+console.log(newgreet("morning"));
+
+
+function calculate(a, b, callBack) {
+    return callBack(a, b)
+}
+
+function callBack (a,b) {
+    return a * b;
+}
+
+let result = calculate(2,4, callBack)
+
+console.log(result);
+
+
